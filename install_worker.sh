4529387d4ee807f59c31c8b460fdd72ce915e076
Eng Cheng <60912855+limecheng@users.noreply.github.com>
2022-10-06 11:53:44 +0800

    updated to kubernetes 1.25

--- install_worker.sh
+++ install_worker.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# Updated: 2022-10-06
 # Source: http://kubernetes.io/docs/getting-started-guides/kubeadm/
 
 ### setup terminal
@@ -14,6 +15,9 @@ echo 'alias c=clear' >> ~/.bashrc
 echo 'complete -F __start_kubectl k' >> ~/.bashrc
 sed -i '1s/^/force_color_prompt=yes\n/' ~/.bashrc
 
+# parameters
+ADVERT_ADDR=172.24.210.132
+KUBE_VERSION=1.25.2
 
 ### install k8s and docker
 apt-get remove -y docker.io kubelet kubeadm kubectl kubernetes-cni
@@ -25,9 +29,8 @@ curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-KUBE_VERSION=1.21.2
 apt-get update
-apt-get install -y docker.io kubelet=${KUBE_VERSION}-00 kubeadm=${KUBE_VERSION}-00 kubectl=${KUBE_VERSION}-00 kubernetes-cni=0.8.7-00
+apt-get install -y docker.io kubelet=${KUBE_VERSION}-00 kubeadm=${KUBE_VERSION}-00 kubectl=${KUBE_VERSION}-00 kubernetes-cni=1.1.1-00
 
 cat > /etc/docker/daemon.json <<EOF
 {
@@ -50,13 +53,29 @@ docker info | grep -i "cgroup"
 
 systemctl enable kubelet && systemctl start kubelet
 
-
 ### init k8s
+rm /root/.kube/config
 kubeadm reset -f
-systemctl daemon-reload
-service kubelet start
+kubeadm init --kubernetes-version=${KUBE_VERSION} --ignore-preflight-errors=NumCPU --skip-token-print --apiserver-advertise-address ${ADVERT_ADDR}
+
+mkdir -p ~/.kube
+sudo cp -i /etc/kubernetes/admin.conf ~/.kube/config
+
+# install weavenet CNI
+kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
+
+# setup aliases
+cat <<EOF > ~/.bashrc
+alias k="kubectl"
+alias kg="kubectl get"
+alias kx="kubectl delete --grace-period=0 --force"
+alias kd="kubectl describe"
+alias kcc="kubectl config current-context"
+alias kcg="kubectl config get-contexts"
+alias kcs="kubectl config set-context"
+EOF
+
 
 echo
-echo "EXECUTE ON MASTER: kubeadm token create --print-join-command --ttl 0"
-echo "THEN RUN THE OUTPUT AS COMMAND HERE TO ADD AS WORKER"
-echo
+echo "### COMMAND TO ADD A WORKER NODE ###"
+kubeadm token create --print-join-command --ttl 0
